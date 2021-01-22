83be9d50a9e43eae7f4e9d8829033c665084b698
Maximilian Ott <maximilian.o.ott@fau.de>
2021-01-22 19:18:18 +0100

    hw::reg: Implement abstraction of ESR
    Implement abstraction of exception syndrome register (ESR).

--- kernel/irq/exception_handler.cc
+++ kernel/irq/exception_handler.cc
@@ -1,85 +1,166 @@
 #include <cstdint.h>
+#include <hw/register/esr.h>
 #include <kernel/irq/exception_handler.h>
 
 extern "C" {
 
 	void current_el_sp_el0_sync(void* saved_state) {
 		(void) saved_state;
+
+		hw::reg::ESR esr;
+		const char* description = esr.getECString();
+		(void) description;
+
 		/* TODO: Implement me */
 	}
 
 	void current_el_sp_el0_irq(void* saved_state) {
 		(void) saved_state;
+
+		hw::reg::ESR esr;
+		const char* description = esr.getECString();
+		(void) description;
+
 		/* TODO: Implement me */
 	}
 
 	void current_el_sp_el0_fiq(void* saved_state) {
 		(void) saved_state;
+
+		hw::reg::ESR esr;
+		const char* description = esr.getECString();
+		(void) description;
+
 		/* TODO: Implement me */
 	}
 
 	void current_el_sp_el0_serror(void* saved_state) {
 		(void) saved_state;
+
+		hw::reg::ESR esr;
+		const char* description = esr.getECString();
+		(void) description;
+
 		/* TODO: Implement me */
 	}
 
 	void current_el_sp_elx_sync(void* saved_state) {
 		(void) saved_state;
+
+		hw::reg::ESR esr;
+		const char* description = esr.getECString();
+		(void) description;
+
 		/* TODO: Implement me */
 	}
 
 	void current_el_sp_elx_irq(void* saved_state) {
 		(void) saved_state;
+
+		hw::reg::ESR esr;
+		const char* description = esr.getECString();
+		(void) description;
+
 		/* TODO: Implement me */
 	}
 
 	void current_el_sp_elx_fiq(void* saved_state) {
 		(void) saved_state;
+
+		hw::reg::ESR esr;
+		const char* description = esr.getECString();
+		(void) description;
+
 		/* TODO: Implement me */
 	}
 
 	void current_el_sp_elx_serror(void* saved_state) {
 		(void) saved_state;
+
+		hw::reg::ESR esr;
+		const char* description = esr.getECString();
+		(void) description;
+
 		/* TODO: Implement me */
 	}
 
 	void lower_el_aarch64_sync(void* saved_state) {
 		(void) saved_state;
+
+		hw::reg::ESR esr;
+		const char* description = esr.getECString();
+		(void) description;
+
 		/* TODO: Implement me */
 	}
 
 	void lower_el_aarch64_irq(void* saved_state) {
 		(void) saved_state;
+
+		hw::reg::ESR esr;
+		const char* description = esr.getECString();
+		(void) description;
+
 		/* TODO: Implement me */
 	}
 
 	void lower_el_aarch64_fiq(void* saved_state) {
 		(void) saved_state;
+
+		hw::reg::ESR esr;
+		const char* description = esr.getECString();
+		(void) description;
+
 		/* TODO: Implement me */
 	}
 
 	void lower_el_aarch64_serror(void* saved_state) {
 		(void) saved_state;
+
+		hw::reg::ESR esr;
+		const char* description = esr.getECString();
+		(void) description;
+
 		/* TODO: Implement me */
 	}
 
 	void lower_el_aarch32_sync(void* saved_state) {
 		(void) saved_state;
+
+		hw::reg::ESR esr;
+		const char* description = esr.getECString();
+		(void) description;
+
 		/* TODO: Implement me */
 	}
 
 	void lower_el_aarch32_irq(void* saved_state) {
 		(void) saved_state;
+
+		hw::reg::ESR esr;
+		const char* description = esr.getECString();
+		(void) description;
+
 		/* TODO: Implement me */
 	}
 
 	void lower_el_aarch32_fiq(void* saved_state) {
 		(void) saved_state;
+
+		hw::reg::ESR esr;
+		const char* description = esr.getECString();
+		(void) description;
+
 		/* TODO: Implement me */
 	}
 
 	void lower_el_aarch32_serror(void* saved_state) {
 		(void) saved_state;
+
+		hw::reg::ESR esr;
+		const char* description = esr.getECString();
+		(void) description;
+
 		/* TODO: Implement me */
 	}
 }
