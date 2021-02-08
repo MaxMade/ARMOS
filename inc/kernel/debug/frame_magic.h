473c17e0771d31c4fa3f5da79688082d8939c2fc
Maximilian Ott <maximilian.o.ott@fau.de>
2021-02-08 17:24:38 +0100

    DEBUG: Generate stack traces
    To provide a better debugging experience, the stack_trace header is
    added.

---
+++ inc/kernel/debug/frame_magic.h
@@ -0,0 +1,15 @@
+#ifndef _INC_KERNEL_DEBUG_FRAME_MAGIC_H_
+#define _INC_KERNEL_DEBUG_FRAME_MAGIC_H_
+
+/**
+ * @file kernel/debug/frame_magic.h
+ * @brief Magic Number for outermost frame
+ */
+
+/**
+ * @def FRAME_MAGIC
+ * @brief Magic number of outermost frame
+ */
+#define FRAME_MAGIC 0x4853
+
+#endif /* ifndef _INC_KERNEL_DEBUG_FRAME_MAGIC_H_ */
