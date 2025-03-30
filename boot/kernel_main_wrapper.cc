019f030cf628a1e57513342173e9d9906662f74b
MaxMade <28571007+MaxMade@users.noreply.github.com>
2025-03-30 10:55:39 +0200

    C++: Call global constructors
    To provide working global constructors, those must be called manually
    before branching to kernelMain.

--- boot/kernel_main_wrapper.cc
+++ boot/kernel_main_wrapper.cc
@@ -1,9 +1,21 @@
+static void csu_init() {
+	extern void (*__init_array_start []) ();
+	extern void (*__init_array_end[]) ();
+
+	long size = __init_array_end - __init_array_start;
+	for (long i = 0; i < size; i++) {
+		(*__init_array_start[i])();
+	}
+}
+
 extern "C" void kernelMainWrapper(void *fdt, int cpuID) {
 	(void) fdt;
 	(void) cpuID;
 
 	if (cpuID == 0) {
-		/* TODO: Call Global constructors */
+		/* Call Global constructors */
+		csu_init();
+
 		/* TODO: Call kernelMain */
 
 	} else {
