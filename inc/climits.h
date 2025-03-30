ceb1bee054762e2df5ae77c2cb64a9969c3cecc8
MaxMade <28571007+MaxMade@users.noreply.github.com>
2025-03-30 10:58:02 +0200

    STL: Implement climits.h header
    To provide implementation-defined value, the climits header is added.

---
+++ inc/climits.h
@@ -0,0 +1,21 @@
+#ifndef _INC_CLIMITS_H_
+#define _INC_CLIMITS_H_
+
+/**
+ * @file climits.h
+ * @brief Implementation-defined constants
+ */
+
+/**
+ * @def PAGESIZE
+ * @brief Size of page
+ */
+#define PAGESIZE 4096
+
+/**
+ * @def PAGE_SIZE
+ * @brief Size of page
+ */
+#define PAGE_SIZE 4096
+
+#endif /* _INC_CLIMITS_H_ */
