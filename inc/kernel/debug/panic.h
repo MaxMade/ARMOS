3169593082caf35600ba8f6659c280cdd3d75214
MaxMade <28571007+MaxMade@users.noreply.github.com>
2025-03-30 10:55:40 +0200

    DEBUG: Use panic::generateFromIRQ to signal panic in trap handler
    To provide accurate information about the origin of the trap, the
    gernerateFromIRQ function will produce output for the saved register
    file and the stack trace for the trapped function.

--- inc/kernel/debug/panic.h
+++ inc/kernel/debug/panic.h
@@ -1,6 +1,8 @@
 #ifndef _INC_KERNEL_DEBUG_PANIC_H_
 #define _INC_KERNEL_DEBUG_PANIC_H_
 
+#include <kernel/irq/exception_handler.h>
+
 /**
  * @file kernel/debug/panic.h
  * @brief Kernel Panic
@@ -19,6 +21,13 @@ namespace debug::panic {
 	 * @warning This will stop the whole system
 	 */
 	void generate(const char msg[]);
+
+	/**
+	 * @fn void generateFromIRQ(const char msg[], irq::ExceptionContext* exceptionContext)
+	 * @brief Generate panic (as viewed in exceptionContext)
+	 * @warning This will stop the whole system
+	 */
+	void generateFromIRQ(const char msg[], irq::ExceptionContext* exceptionContext);
 }
 
 #endif /* ifndef _INC_KERNEL_DEBUG_PANIC_H_ */
