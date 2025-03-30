df5f7fd91f6fd6205778053fb6ae2877a43c79a3
MaxMade <28571007+MaxMade@users.noreply.github.com>
2025-03-30 10:55:39 +0200

    LINKER: Implement linker header
    To deal with variables defined in the linker script, the linker header
    is added.

---
+++ kernel/linker.cc
@@ -0,0 +1,47 @@
+#include <cstdint.h>
+#include <kernel/linker.h>
+
+extern uintptr_t __TEXT_START;
+extern uintptr_t __TEXT_END;
+
+extern uintptr_t __RODATA_START;
+extern uintptr_t __RODATA_END;
+
+extern uintptr_t __DATA_START;
+extern uintptr_t __DATA_END;
+
+extern uintptr_t __BSS_START;
+extern uintptr_t __BSS_END;
+
+
+lib::pair<void*, size_t> linker::getTextSegment() {
+	void* start = &__TEXT_START;
+	void* end = &__TEXT_END;
+	size_t size = reinterpret_cast<uintptr_t>(end) - reinterpret_cast<uintptr_t>(start);
+
+	return lib::pair(start, size);
+}
+
+lib::pair<void*, size_t> linker::getRODataSegment() {
+	void* start = &__RODATA_START;
+	void* end = &__RODATA_END;
+	size_t size = reinterpret_cast<uintptr_t>(end) - reinterpret_cast<uintptr_t>(start);
+
+	return lib::pair(start, size);
+}
+
+lib::pair<void*, size_t> linker::getDataSegment() {
+	void* start = &__DATA_START;
+	void* end = &__DATA_END;
+	size_t size = reinterpret_cast<uintptr_t>(end) - reinterpret_cast<uintptr_t>(start);
+
+	return lib::pair(start, size);
+}
+
+lib::pair<void*, size_t> linker::getBSSSegment() {
+	void* start = &__BSS_START;
+	void* end = &__BSS_END;
+	size_t size = reinterpret_cast<uintptr_t>(end) - reinterpret_cast<uintptr_t>(start);
+
+	return lib::pair(start, size);
+}
