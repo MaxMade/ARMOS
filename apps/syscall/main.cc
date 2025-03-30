854f748ce1233108cb19d620663c2366ad090c43
MaxMade <28571007+MaxMade@users.noreply.github.com>
2025-03-30 10:55:40 +0200

    BUILD: Separate user and kernel headers
    To provide separation of user and kernel space headers during the build
    process, the build infrastructure is extended.

---
+++ apps/syscall/main.cc
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+extern "C" int main(void) {
+
+	/* Perform write system call */
+	const char* msg =  "Hallo Welt!";
+	size_t len = 11;
+	syscall(4, 1, msg, len);
+
+	while (1);
+	return 0;
+}
