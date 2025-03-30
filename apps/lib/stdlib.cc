b774feadace4336e747e653fd6d219868ad2262e
MaxMade <28571007+MaxMade@users.noreply.github.com>
2025-03-30 10:55:41 +0200

    APP: Add stdlib.h
    To provide access to the standard libary definitions, the stdlib.h is
    added.

---
+++ apps/lib/stdlib.cc
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <string.h>
+#include <__ListAllocator.h>
+
+/* Allocator */
+static const size_t ALLOCATOR_SIZE (64 * 1024 * 1024);
+static char mem[ALLOCATOR_SIZE];
+static ListAllocator<32, BestFit> allocator(mem, ALLOCATOR_SIZE);
+
+/* Locking */
+typedef bool lock_t;
+static lock_t allocator_lock = false;
+
+static void lock() {
+	while(__atomic_test_and_set(&allocator_lock, __ATOMIC_SEQ_CST));
+}
+
+static void unlock() {
+	__atomic_clear(&allocator_lock, __ATOMIC_SEQ_CST);
+}
+
+
+void *malloc(size_t size) {
+	lock();
+	auto ret = allocator.allocate(size);
+	unlock();
+	return ret;
+}
+
+void free(void *ptr) {
+	lock();
+	allocator.deallocate(ptr);
+	unlock();
+}
+
+void *calloc(size_t nmemb, size_t size) {
+	lock();
+	auto ret = allocator.allocate(nmemb * size);
+	if (ret != nullptr) {
+		memset(ret, 0, nmemb * size);
+	}
+	unlock();
+	return ret;
+}
--- kernel/apps/constructor.cc
+++ kernel/apps/constructor.cc
@@ -2,17 +2,14 @@
 #include <kernel/apps/constructor.h>
 
 extern int main();
-extern uint32_t *__app_init_array_start [];
-extern uint32_t *__app_init_array_end[];
+extern void (*__app_init_array_start []) ();
+extern void (*__app_init_array_end[]) ();
 
 
 int apps::callConstructorAndMain() {
 	long size = __app_init_array_end - __app_init_array_start;
 	for (long i = 0; i < size; i++) {
-		/* XXX: Ugly casts and unstable */
-		auto tmp = reinterpret_cast<uintptr_t>(__app_init_array_start[i]) >> 32;
-		auto fn = reinterpret_cast<void(*)()>(tmp);
-		fn();
+		(*__app_init_array_start[i])();
 	}
 
 	return main();
